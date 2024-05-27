ab733629ef2bdb94389b4272968591eb29ddedad
JoshBenn <jdbenn@asu.edu>
2024-05-26 20:47:25 -0700

    expanding more on js server side

--- JS/server.js
+++ JS/server.js
@@ -1,7 +1,52 @@
 import { createServer } from "node:http";
+import { argv } from "node:process";
 
-const hostname = "localhost";
-const port = "8000"
+const remote = "";
+const local = "localhost:8000";
+const mode = Mode.node;
+const currentMode = Arguments.none;
+
+argv.slice(0).forEach(arg => {
+    switch (currentMode) {
+        case Arguments.modeArg: {
+            if (arg.toLowerCase() === "client") {
+                mode = Mode.client;
+            }
+            break;
+        }
+        case Arguments.localArg: {
+            let values = arg.split(":");
+            break;
+        }
+        case Arguments.remoteArg: {
+
+            break;
+        }
+    }
+    switch (Arguments(arg.toLowerCase())) {
+        case Arguments.modeArg:
+            currentMode = Arguments.modeArg;
+            break;
+        case Arguments.localArg:
+            currentMode = Arguments.localArg;
+            break;
+        case Arguments.remoteArg:
+            currentMode = Arguments.remoteArg;
+            break;
+        default:
+            currentMode = Arguments.none;
+            break;
+    }
+});
+
+const Arguments = {
+    modeArg: "-m" | "--mode",
+    localArg: "-l" | "--local",
+    remoteArg: "-r" | "--remote",
+    none,
+};
+
+const Mode = client | node;
 
 const server = createServer((req, res) => {
     res.statusCode = 200;
@@ -10,5 +55,5 @@ const server = createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-    console.log(`Server running at http://${hostname}:${port}/`)
+    console.log(`Server running at http://${local}:${port}/`)
 });
