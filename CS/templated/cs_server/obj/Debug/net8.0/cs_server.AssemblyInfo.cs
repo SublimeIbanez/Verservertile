83a25348b32b73cd1fe879d6fe3eb242daec29b9
JoshBenn <sublimeibanez@protonmail.com>
2024-07-25 05:28:20 -0700

    Relocated the program into new directory

--- CS/templated/cs_server/obj/Debug/net8.0/cs_server.AssemblyInfo.cs
+++ CS/templated/cs_server/obj/Debug/net8.0/cs_server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cs_server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfb70bc2cde827d4b56c4a00c8ffbef5b45e20ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e50eac6bfd3064b3423832056f99670d05980f61")]
 [assembly: System.Reflection.AssemblyProductAttribute("cs_server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cs_server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
